c8177854e7db5f9208a061f3802f9bead869633c
Jose Borja <chakaitos@gmail.com>
2015-12-29 23:16:05 -0500

    Adds validations for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product
   include Mongoid::Document
-  field :name, type: String,
-  field :type, type: String,
-  field :length, type: Integer,
-  field :width, type: Integer,
-  field :height, type: Integer,
+  field :name, type: String
+  field :type, type: String
+  field :length, type: Integer
+  field :width, type: Integer
+  field :height, type: Integer
   field :weight, type: Integer
+
+  validates :name, :type, :length, :width, :height, :weight, presence: true
+  validates :name, uniqueness: true
 end
