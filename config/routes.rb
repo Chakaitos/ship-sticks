9ff59368f922e885a36859f345fde952b469da1b
Jose Borja <chakaitos@gmail.com>
2015-12-30 09:54:52 -0500

    Removes show by id for conflicting with dimensions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :products
+  resources :products, except: :show
   get '/products/dimensions/:length/:width/:height/:weight' => 'products#find_best_match'
 
   root 'products#index'
