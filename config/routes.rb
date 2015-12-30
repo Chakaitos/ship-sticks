bfa3cfca234f4d89ce2d83bdb5bcc089385ea595
Jose Borja <chakaitos@gmail.com>
2015-12-30 02:37:08 -0500

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
-  get '/products/:length/:width/:height/:weight' => 'products#find_best_match'
+  get '/products/dimensions/:length/:width/:height/:weight' => 'products#find_best_match'
 
   root 'products#index'
 end
