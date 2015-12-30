53327da3be7111f2deb7b3253b8b165aada912be
Jose Borja <chakaitos@gmail.com>
2015-12-30 02:18:40 -0500

    Scopes not working needs troubleshooting

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,10 +10,10 @@ class Product
   validates :name, :type, :length, :width, :height, :weight, presence: true
   validates :name, uniqueness: true
 
-  scope :best_length, ->(length) { where("length >= ?", length).order('length asc') }
-  scope :best_width, ->(width) { where("width >= ?", width).order('width asc') }
-  scope :best_height, ->(height) { where("height >= ?", height).order('height asc') }
-  scope :best_weight, ->(weight) { where("weight >= ?", weight).order('weight asc') }
+  scope :best_length, ->(l) { where("length >= ?", l).order('length asc') }
+  scope :best_width, ->(w) { where("width >= ?", w).order('width asc') }
+  scope :best_height, ->(h) { where("height >= ?", h).order('height asc') }
+  scope :best_weight, ->(w) { where("weight >= ?", w).order('weight asc') }
 
   def self.best_match length, width, height, weight
     self.best_length(length).best_width(width).best_height(height).best_weight(weight)
