0ef0301fa41df61056c8215b09057c95b6ac4e2c
Jose Borja <chakaitos@gmail.com>
2015-12-29 22:43:38 -0500

    Adds exposures for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+
+  expose(:id)       { params[:id] }
+  expose(:name)     { params[:name] }
+  expose(:type)     { params[:type] }
+  expose(:length)   { params[:length] }
+  expose(:width)    { params[:width] }
+  expose(:height)   { params[:height] }
+  expose(:weight)   { params[:weight] }
+  expose(:product)  { Product.find(id) }
+  expose(:products) { Product.all }
+
 end
